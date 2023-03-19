fc4d9e74376b3aea2302a88b35d51cb9be018d41
afate <panzhongbo@outlook.com>
2023-03-20 01:23:52 +0800

    profile-2

--- src/clj/guestbook/messages.clj
+++ src/clj/guestbook/messages.clj
@@ -11,12 +11,15 @@
 ;;                      :errors errors}))
 ;;     (db/save-message! message)))
 
-(defn save-message! [{:keys [login]} message]
+(defn save-message! [{{:keys [display-name]}
+                      :profile
+                      :keys [login]}
+                     message]
   (if-let [errors (validate-message message)]
     (throw (ex-info "Message is invalid"
                     {:guestbook/error-id :validation
                      :errors errors}))
-    (db/save-message! (assoc message :author login))))
+    (db/save-message! (assoc message :author login :name (or display-name login)))))
 
 (defn messages-by-author [author]
   {:messages (vec (db/get-messages-by-author {:author author}))})
